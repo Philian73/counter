5acb5f072e5798d80a3349663694d94ae9e41894
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-29 01:09:06 +0400

    'Wrapper' style and displayed counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './styles/App.css'
+import { Counter } from './components/Counter/Counter.tsx'
 
 const App = () => {
-  return <div></div>
+  return (
+    <div className="wrapper">
+      <Counter min={5} max={12} />
+    </div>
+  )
 }
 
 export default App
