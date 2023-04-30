423eb0722ccbdef2b96977c8dd5588bcba2a4602
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-30 23:32:48 +0400

    Created a common file with the names of buttons and errors

---
+++ src/components/Counter/counter.ts
@@ -0,0 +1,20 @@
+type ButtonsNameType = {
+  inc: string
+  reset: string
+  set: string
+}
+
+type ErrorsStatusType = {
+  incorrect: string
+  change: string
+}
+
+export const BUTTONS_NAME: ButtonsNameType = {
+  inc: 'inc',
+  reset: 'reset',
+  set: 'set',
+}
+export const ERRORS_STATUS: ErrorsStatusType = {
+  incorrect: 'incorrect value',
+  change: 'value change',
+}
